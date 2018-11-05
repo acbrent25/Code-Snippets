ea8c7d852f214ea1c352d7c0836405aa16841fc1
Adam Champagne <achampagne@sussmanconsultants.com>
2018-11-05 16:59:56 -0500

    gravity forms php

--- wordpress/gravity-forms/gravity-forms.php
+++ wordpress/gravity-forms/gravity-forms.php
@@ -1,2 +1,8 @@
+<?php 
+
 // On error redirect to form
 add_filter( 'gform_confirmation_anchor', '__return_true' );
+
+
+// Hide Gravity Form field labels when using placeholders.
+add_filter( 'gform_enable_field_label_visibility_settings', '__return_true' );
