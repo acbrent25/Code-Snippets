a207308ffdcab5de48244043d146bba05edd0125
Adam Champagne <achampagne@sussmanconsultants.com>
2018-11-07 15:57:46 -0500

    commented code

--- jQuery/customFormErrorMessage/customFormErrorMessages.js
+++ jQuery/customFormErrorMessage/customFormErrorMessages.js
@@ -1,23 +1,31 @@
 jQuery(document).ready(function($){
 	console.log('ready');
 	
-// Require Event Type and send error if empty
+// Submit Button Function to Check for Empty Fields
 $('.sub-btn').click(function(e){
+   // Prevent default click behavoir
    e.preventDefault();
-   // Get Form ID
+   // Create alert message array to store all alerts
    var alertMessage = [];
+   // empty alert for use in loop
    var alert = '';
+   // if event type is missing a value then push custom alertmessage to array
    if ($('#event_type_id').val() === ''){
       alertMessage.push("Please Select an Event Type");
    }
-   if($('#widget_type_id').val() === ''){
+   // Same as above
+   if ($('#widget_type_id').val() === ''){
       alertMessage.push("Please Select a Widget Type");
    } 
- 
-   if(alertMessage != undefined || alertMessage != 0){
+   
+   // fi the alert message array isn't empty
+   if (alertMessage != undefined || alertMessage != 0){
+      //loop over ever alertmessage in array
       for(var i = 0; i < alertMessage.length; i++){
          console.log("alert message " + alertMessage[i]);
+         // add alertMessages to alert for printing to DOM
          alert += alertMessage[i] + '<br>';
+         // Add to DOM element and show
          $('.alert').html(alert).show();
       }
    }
