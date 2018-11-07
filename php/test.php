5af1c374944a1ea8c85b580052e3162c1cf47e80
Adam Champagne <achampagne@sussmanconsultants.com>
2018-11-07 14:57:41 -0500

    jquery

---
+++ php/test.php
@@ -0,0 +1,9 @@
+<?php
+
+// Show content if test variable is present.  Usefull for testing front end code before going live
+
+if (isset($_GET["test"])) { ?>
+
+   <h1>Your code here</h1>
+
+<?php } ?>
