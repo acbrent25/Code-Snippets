6159c20cdd214499d2746ed049c928639de3ec77
Adam Champagne <achampagne@sussmanconsultants.com>
2018-11-07 15:54:01 -0500

    made error loop to print all errors

--- jQuery/customFormErrorMessage/customFormErrorMessages.js
+++ jQuery/customFormErrorMessage/customFormErrorMessages.js
@@ -2,19 +2,27 @@ jQuery(document).ready(function($){
 	console.log('ready');
 	
 // Require Event Type and send error if empty
-$('.sub-btn').click(function(){
-   
+$('.sub-btn').click(function(e){
+   e.preventDefault();
    // Get Form ID
-   var event_type = $('#event_type_id');
    var alertMessage = [];
-   
-   if (event_type.val() === ''){
+   var alert = '';
+   if ($('#event_type_id').val() === ''){
       alertMessage.push("Please Select an Event Type");
-      $('.alert').text(alertMessage).show();
-      return false;
+   }
+   if($('#widget_type_id').val() === ''){
+      alertMessage.push("Please Select a Widget Type");
    } 
-   
-   else return;
+ 
+   if(alertMessage != undefined || alertMessage != 0){
+      for(var i = 0; i < alertMessage.length; i++){
+         console.log("alert message " + alertMessage[i]);
+         alert += alertMessage[i] + '<br>';
+         $('.alert').html(alert).show();
+      }
+   }
+
+
 });
 	
 });
