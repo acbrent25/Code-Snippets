2fc1a0ca5e74eae23c5d945cceeec12e630fcbfb
Adam Champagne <achampagne@sussmanconsultants.com>
2018-11-12 16:18:23 -0500

    github setup

---
+++ jQuery/findByValue.js
@@ -0,0 +1,9 @@
+// Find any input or button by value.
+
+// jQuery
+$('input[type="button"][value="My Button"]');
+// Or
+$('input[type="text"][value="My Text Input"]');
+
+// Plain Javascript
+document.querySelectorAll("input[value=My Text Input]");
